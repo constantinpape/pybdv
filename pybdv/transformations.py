4a58710ef96232fc41c0353226f1ec393f97597a
Constantin Pape <c.pape@gmx.net>
2019-06-18 06:50:33 +0200

    Update transformation helpers

--- pybdv/transformations.py
+++ pybdv/transformations.py
@@ -1,9 +1,55 @@
 import xml.etree.ElementTree as ET
 import numpy as np
+from .metadata import indent_xml
 
 
+#
+# switch between transformation vector and affine transformation matrix
+# Affine Matrix:
+# [[a00 a01 a02 a03]
+#  [a10 a11 a12 a13]
+#  [a20 a21 a22 a23]
+#  [  0   0   0   1]]
+# Transformation Vector (as stored by bdv)
+# [a00 a01 a02 a03 a10 a11 a12 a13 a20 a21 a22 a23]
+#
+
+
+def transformation_to_matrix(transformation):
+    """ From bdv transformation vector to affine matrix.
+    """
+    matrix = np.zeros((4, 4))
+
+    matrix[0, :] = transformation[0:4]
+    matrix[1, :] = transformation[4:8]
+    matrix[2, :] = transformation[8:12]
+
+    matrix[3, 3] = 1
+    return matrix
+
+
+def matrix_to_transformation(matrix):
+    """ From affine matrix to bdv transformation vector.
+    """
+    expected_last_row = np.array([0, 0, 0, 1])
+    assert matrix.shape == (4, 4)
+    assert (matrix[:, 3] == expected_last_row).all()
+
+    transformation = np.zeros(12)
+    transformation[0:4] = matrix[0, :]
+    transformation[4:8] = matrix[1, :]
+    transformation[8:12] = matrix[2, :]
+
+    return transformation
+
+
+#
+# read and write transformations from / to xml
 # TODO support multiple set-ups / time-points
-def load_resolution_and_transformation(xml):
+#
+
+
+def read_resolution_and_transformation(xml):
     tree = ET.ElementTree(file=xml)
 
     for elem in tree.iter():
@@ -22,7 +68,6 @@ def load_resolution_and_transformation(xml):
     return resolution, transformation
 
 
-# TODO support multiple set-ups / time-points
 def write_resolution_and_transformation(xml, resolution, transformation):
     assert len(resolution) == 3
     assert len(transformation) == 12
@@ -42,51 +87,19 @@ def write_resolution_and_transformation(xml, resolution, transformation):
             for child in elem:
                 child.text = trafo_string
 
+    root = tree.getroot()
+    indent_xml(root)
+    tree = ET.ElementTree(root)
+    tree.write(xml)
+
 
 def write_resolution_and_matrix(xml, resolution, matrix):
     transformation = matrix_to_transformation(matrix)
     write_resolution_and_transformation(xml, resolution, transformation)
 
 
-# TODO figure out row-major etc. here
-def transformation_to_matrix(transformation):
-    matrix = np.zeros((4, 4))
-
-    matrix[0, 0] = transformation[0]
-    matrix[0, 1] = transformation[1]
-    matrix[0, 2] = transformation[2]
-    matrix[0, 3] = transformation[3]
-
-    matrix[1, 0] = transformation[4]
-    matrix[1, 1] = transformation[5]
-    matrix[1, 2] = transformation[6]
-    matrix[1, 3] = transformation[7]
-
-    matrix[2, 0] = transformation[8]
-    matrix[2, 1] = transformation[9]
-    matrix[2, 2] = transformation[10]
-    matrix[2, 3] = transformation[11]
-
-    matrix[3, 3] = 1
-    return matrix
-
-
-def matrix_to_transformation(matrix):
-    expected_col = np.array([0, 0, 0, 1])
-    assert matrix.shape == (4, 4)
-    assert (matrix[3, :] == expected_col).all()
-    assert (matrix[:, 3] == expected_col).all()
-
-    transformation = np.zeros(12)
-    transformation[0:4] = matrix[0, :]
-    transformation[4:8] = matrix[1, :]
-    transformation[8:12] = matrix[2, :]
-
-    return transformation
-
-
-def load_resolution_and_matrix(xml):
-    resolution, transformation = load_resolution_and_transformation(xml)
+def read_resolution_and_matrix(xml):
+    resolution, transformation = read_resolution_and_transformation(xml)
     matrix = transformation_to_matrix(transformation)
     return resolution, matrix
 
@@ -95,9 +108,10 @@ def load_resolution_and_matrix(xml):
 # https://math.stackexchange.com/questions/237369/given-this-transformation-matrix-how-do-i-decompose-it-into-translation-rotati
 def get_translation_from_matrix(matrix):
     t = np.zeros((4, 4))
-    # TODO [0, i] or [i, 0] ???
     for i in range(3):
-        t[i, 0] = matrix[i, 0]
+        t[i, 3] = matrix[i, 3]
+        t[i, i] = 1
+    t[3, 3] = 1
     return t
 
 
@@ -105,6 +119,7 @@ def get_scaling_from_matrix(matrix):
     s = np.zeros((4, 4))
     for i in range(3):
         s[i, i] = np.linalg.norm(matrix[:, i])
+    s[3, 3] = 1
     return s
 
 
@@ -113,19 +128,10 @@ def get_rotation_and_shear_from_matrix(matrix):
 
     # need scaling to compute rotation/shear
     s = get_scaling_from_matrix(matrix)
-    sx, sy, sz = s[0, 0], s[1, 1], s[2, 2]
+    s = np.array([s[0, 0], s[1, 1], s[2, 2]])
 
-    r[0, 0] = matrix[0, 0] / sx
-    r[0, 1] = matrix[0, 1] / sy
-    r[0, 2] = matrix[0, 2] / sz
-
-    r[1, 0] = matrix[1, 0] / sx
-    r[1, 1] = matrix[1, 1] / sy
-    r[1, 2] = matrix[1, 2] / sz
-
-    r[2, 0] = matrix[2, 0] / sx
-    r[2, 1] = matrix[2, 1] / sy
-    r[2, 2] = matrix[2, 2] / sz
+    for i in range(3):
+        r[i, :3] = matrix[i, :3] / s
 
     r[3, 3] = 1
     return r
