21db3fa667721267a7e9b5dc91384cb17314a2da
Constantin Pape <c.pape@gmx.net>
2020-03-28 11:43:42 +0100

    Update converter script

--- pybdv/scripts/pybdv_converter.py
+++ pybdv/scripts/pybdv_converter.py
@@ -5,27 +5,43 @@ import json
 from pybdv import convert_to_bdv
 
 
-# TODO update the arguments
 def main():
-    parser = argparse.ArgumentParser(description='Convert hdf5 dataset to bigdataviewer format.')
+    parser = argparse.ArgumentParser(description='Convert dataset from container (hdf5 or n5) to bigdataviewer format.')
+
     parser.add_argument('input_path', type=str,
                         help='path to the hdf5 input file')
     parser.add_argument('input_key', type=str,
                         help='path in file to input dataset')
     parser.add_argument('output_path', type=str,
                         help='path to the output file')
+
     parser.add_argument('--downscale_factors', type=str, default=None,
-                        help='factors used for creating downscaled image pyramid, needs to be encoded as json list')
+                        help='factors used for creating downscaled image pyramid, expects json encoded list')
     parser.add_argument('--downscale_mode', type=str, default='nerarest',
                         help='mode used for downscaling, can be nearest, mean, max, min or interpolate')
     parser.add_argument('--resolution', type=float, nargs=3, default=[1., 1., 1.],
                         help='resolution of the data')
     parser.add_argument('--unit', type=str, default='pixel',
                         help='unit of measurement')
+
     parser.add_argument('--setup_id', type=int, default=None,
                         help='id of the setup to write')
+    parser.add_argument('--timepoint', type=int, default=0,
+                        help='timepoint id to write')
     parser.add_argument('--setup_name', type=str, default=None,
                         help='name of the setup to write')
+
+    parser.add_argument('--affine', type=str, default=None,
+                        help='affine transformation(s) used for view registration, expects json encoded list or dict')
+    default_attributes = {'channel': None}
+    default_attributes = json.dumps(default_attributes)
+    parser.add_argument('--attributes', type=str, default=default_attributes,
+                        help='attributes for this view, expects json encoded dict')
+
+    parser.add_argument('--overwrite', type=int, default=0,
+                        help='whether to overwrite existing views')
+    parser.add_argument('--chunks', type=str, default=None,
+                        help='chunk settings, expects json encoded list')
     parser.add_argument('--n_threads', type=int, default=1,
                         help='number of threads used for copying and downsampling')
 
@@ -40,11 +56,39 @@ def main():
         except ValueError as e:
             raise ValueError("Decoding downscale_factors as json failed with %s" % str(e))
 
+    # decode affine
+    if args.affine is None:
+        affine = None
+    else:
+        try:
+            affine = json.loads(args.affine)
+        except ValueError as e:
+            raise ValueError("Decoding affine as json failed with %s" % str(e))
+
+    # decode attributes
+    if args.attributes is None:
+        attributes = None
+    else:
+        try:
+            attributes = json.loads(args.attributes)
+        except ValueError as e:
+            raise ValueError("Decoding attributes as json failed with %s" % str(e))
+
+    # decode chunks
+    if args.chunks is None:
+        chunks = None
+    else:
+        try:
+            chunks = json.loads(args.chunks)
+        except ValueError as e:
+            raise ValueError("Decoding chunks as json failed with %s" % str(e))
+
     convert_to_bdv(args.input_path, args.input_key, args.output_path,
                    downscale_factors, args.downscale_mode,
                    resolution=args.resolution, unit=args.unit,
-                   setup_id=args.setup_id, setup_name=args.setup_name,
-                   n_threads=args.n_threads)
+                   setup_id=args.setup_id, timepoint=args.timepoint, setup_name=args.setup_name,
+                   affine=affine, attributes=attributes, overwrite=int(args.overwrite),
+                   chunks=chunks, n_threads=args.n_threads)
 
 
 if __name__ == '__main__':
--- test/test_convert_to_bdv.py
+++ test/test_convert_to_bdv.py
@@ -10,11 +10,16 @@ from pybdv.util import get_key, open_file, HAVE_ELF
 
 class ConvertToBdvTestMixin(ABC):
     tmp_folder = './tmp'
+    xml_path = './tmp/test.xml'
+
+    def _make_input(self, name, data, chunks):
+        with open_file(self.in_path, 'a') as f:
+            f.create_dataset(name, data=data, chunks=chunks)
 
     def setUp(self):
         os.makedirs(self.tmp_folder, exist_ok=True)
-        with open_file(self.in_path, 'a') as f:
-            f.create_dataset('data', data=np.random.rand(64, 64, 64), chunks=(32, 32, 32))
+        shape = (64,) * 3
+        self._make_input('data', np.random.rand(*shape), (32,) * 3)
 
     def tearDown(self):
         try:
@@ -39,7 +44,48 @@ class ConvertToBdvTestMixin(ABC):
         convert_to_bdv(self.in_path, 'data', self.out_path)
         self.check_result()
 
-    def test_cli(self):
+    def test_overwrite(self):
+        from pybdv import convert_to_bdv
+        from pybdv.util import get_scale_factors, absolute_to_relative_scale_factors
+        from pybdv.metadata import get_attributes
+
+        def _check(name, exp_sf, exp_attrs):
+            with open_file(self.in_path, 'r') as f:
+                exp_data = f[name][:]
+
+            key = get_key(self.is_h5, timepoint=0, setup_id=0, scale=0)
+            with open_file(self.out_path, 'r') as f:
+                data = f[key][:]
+            self.assertTrue(np.allclose(data, exp_data))
+
+            sf = get_scale_factors(self.out_path, setup_id=0)
+            sf = absolute_to_relative_scale_factors(sf)
+            self.assertEqual(sf, [[1, 1, 1]] + exp_sf)
+
+            attrs = get_attributes(self.xml_path, setup_id=0)
+            self.assertEqual(attrs, exp_attrs)
+
+        sf1 = [[2, 2, 2]]
+        attrs1 = {'channel': 1, 'angle': 2}
+
+        sf2 = [[1, 2, 2], [2, 2, 2]]
+        attrs2 = {'channel': 3, 'angle': 6}
+
+        convert_to_bdv(self.in_path, 'data', self.out_path, setup_id=0, timepoint=0,
+                       downscale_factors=sf1, attributes=attrs1)
+        _check('data', sf1, attrs1)
+
+        self._make_input('data2', np.random.rand(72, 72, 72), (32, 32, 32))
+
+        convert_to_bdv(self.in_path, 'data2', self.out_path, setup_id=0, timepoint=0,
+                       downscale_factors=sf2, attributes=attrs1)
+        _check('data', sf1, attrs1)
+
+        convert_to_bdv(self.in_path, 'data2', self.out_path, setup_id=0, timepoint=0,
+                       downscale_factors=sf2, attributes=attrs2, overwrite=True)
+        _check('data2', sf2, attrs2)
+
+    def test_cli_simple(self):
         call(['convert_to_bdv', self.in_path, 'data', self.out_path])
         self.check_result()
 
