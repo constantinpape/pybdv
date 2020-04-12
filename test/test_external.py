ca02f46bf3b3f2397cb766ce7735c44a03051312
Constantin Pape <c.pape@gmx.net>
2020-04-12 16:25:01 +0200

    Make test_external more robust

--- test/test_external.py
+++ test/test_external.py
@@ -10,16 +10,14 @@ import skimage.data
 
 # dummy test data, needs to be converted to bdv/xml
 # with FIJI externally
-def make_test_data(write=False):
+def make_test_data():
     d = skimage.data.astronaut()
     d = d.transpose((2, 0, 1))
     d = np.concatenate(2 * [d], axis=0)
     d = d.astype('float32')
     d *= (np.iinfo('uint16').max / np.iinfo('uint8').max)
     d = d.astype('uint16')
-    if write:
-        imageio.volwrite('../data/example.tif', d)
-    return d
+    imageio.volwrite('../data/example.tif', d)
 
 
 class TestExternal(unittest.TestCase):
@@ -36,7 +34,7 @@ class TestExternal(unittest.TestCase):
 
     def test_external(self):
         from pybdv import make_bdv
-        d = make_test_data()
+        d = imageio.volread('../data/example.tif')
         res_path = os.path.join(self.tmp_folder, 'data.h5')
         exp_path = '../data/example.h5'
         make_bdv(d, res_path, convert_dtype=True)
@@ -54,5 +52,5 @@ class TestExternal(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    # make_test_data(True)
+    # make_test_data()
     unittest.main()
